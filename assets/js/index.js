333f96a95048a00fb0c0f6dfd8d4465caa866761
Serzh Ayvazyan <ayvazyanserj3@gmail.com>
2025-10-24 16:51:25 +0400

    first

--- assets/js/index.js
+++ assets/js/index.js
@@ -53,3 +53,26 @@ if (parallaxBlocks.length > 0) {
     });
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedItems = document.querySelectorAll(
+    ".fade-left, .fade-right, .fade-top, .fade-bottom"
+  );
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+        
+        }
+      });
+    },
+    {
+      threshold: 0.15, 
+    }
+  );
+
+  animatedItems.forEach((item) => observer.observe(item));
+});
